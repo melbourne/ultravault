2d31c7c904cd4689ffae1ff83fbdf85a48b3db5e
Andrew Turner <andrew.turner@melbourne.co.uk>
2012-02-03 10:09:21 +0000

    Group methods

--- lib/ultravault/group.rb
+++ lib/ultravault/group.rb
@@ -2,6 +2,35 @@ module UltraVault
   
   class Group < Base
     
+    def initialize(client, uuid)
+      @client = client
+      @uuid = uuid
+    end
+    
+    def uuid
+      @uuid
+    end
+    
+    def name
+      @client.get('Name', uuid)
+    end
+    
+    def description
+      @client.get('Description', uuid)
+    end
+    
+    def group_agent_permissions
+      @client.get('GroupAgentPermissions', uuid)
+    end
+    
+    def volume_ids
+      @client.get('VolumeIDs', uuid)
+    end
+    
+    def user_ids
+      @client.get('UserIDs', uuid)
+    end
+    
   end
   
 end
