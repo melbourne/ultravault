1d12d0e607a7e64bd4e84c2ba3571f132732c8d4
Andrew Turner <andrew.turner@melbourne.co.uk>
2012-03-14 16:13:07 +0000

    Remove unnecessary methods

--- lib/ultravault/client.rb
+++ lib/ultravault/client.rb
@@ -7,13 +7,6 @@ module UltraVault
       @client = Savon::Client.new(params[:url])
       @client.connect params[:username], params[:password]
     end
-    
-    def get(attribute)
-      @client.call("#{get}")
-    end
-    
-    def set
-    end
   end
   
 end
