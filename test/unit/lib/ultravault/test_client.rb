1cd181f37dfff9ed3ee2dd409c2d82f456a21e74
Sean Handley <sean.handley@melbourne.co.uk>
2012-03-14 08:31:27 +0000

    Refactored the test.

--- test/unit/lib/ultravault/test_client.rb
+++ test/unit/lib/ultravault/test_client.rb
@@ -10,12 +10,12 @@ class ClientTest < Test::Unit::TestCase
     setup do
       @options = { url: "some.valid.url", username: "foo", password: "bar" }
       @mock_client = mock
-      @mock_client.expects(:connect).with(@options[:username], @options[:password])
-      Savon::Client.expects(:new).with("some.valid.url").returns(@mock_client)
     end
     
     context '#initialize' do
-      should "pass off url to savon client instance" do
+      should "pass off url to savon client instance and call connect" do
+        @mock_client.expects(:connect).with(@options[:username], @options[:password])
+        Savon::Client.expects(:new).with(@options[:url]).returns(@mock_client)
         UltraVault::Client.new @options
       end
     end
