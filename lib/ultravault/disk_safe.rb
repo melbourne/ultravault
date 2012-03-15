3835d1d9bd787476c5cdb4edc9a135877deda417
Sean Handley <sean.handley@gmail.com>
2012-03-15 15:48:29 +0000

    More tests for the disk safe.

--- lib/ultravault/disk_safe.rb
+++ lib/ultravault/disk_safe.rb
@@ -1,7 +1,9 @@
 module UltraVault
   class DiskSafe
     attr_reader :agent_id, :compression_level, :compression_type,
-                  :description, :device_count, :device_list
+                  :description, :device_count, :device_list,
+                  :id, :open, :recovery_point_count,
+                  :volume_id
     
     def initialize(params)
       @agent_id = params[:agent_id]
@@ -10,16 +12,37 @@ module UltraVault
       @description = params[:description]
       @device_count = params[:device_count].to_i
       @device_list = DeviceList.new(params[:device_list])
+      @id = params[:id]      
+      @open = params[:open]                        
+      @recovery_point_count = params[:recovery_point_count].to_i
+      @size = params[:size].to_i  
+      @size_of_deltas = params[:size_of_deltas_in_disk_safe]
+      @volume_id = params[:volume_id]
+    end                         
+    
+    def size
+      ActionController::Base.helpers.
+        number_to_human_size(@size)      
+    end  
+    
+    def size_of_deltas
+      ActionController::Base.helpers.
+        number_to_human_size(@size_of_deltas)      
     end
   
   private
     require 'action_controller'
     class DeviceList
-      attr_reader :content_type
+      attr_reader :content_type, :path, :enabled,
+                  :mount_point, :mounted
       
       def initialize(params)
         @capacity = params[:capacity].to_i
         @content_type = params[:device_content_type]
+        @path = params[:device_path] 
+        @enabled = params[:enabled]
+        @mount_point = params[:mount_point]
+        @mounted = params[:mounted]
       end
       
       def capacity
--- test/unit/lib/ultravault/test_disk_safe.rb
+++ test/unit/lib/ultravault/test_disk_safe.rb
@@ -68,7 +68,7 @@ class DiskSafeTest < Test::Unit::TestCase
           @device_list = @disk_safe.device_list
         end
         
-        should "match the capacity at the write byte level" do
+        should "match the capacity at the right byte level" do
           assert_equal @device_list.capacity, "60 GB"
         end
         
@@ -76,7 +76,65 @@ class DiskSafeTest < Test::Unit::TestCase
           assert_equal @device_list.content_type,
             @input[:device_list][:device_content_type]
         end
+        
+        should "match the device path" do
+          assert_equal @device_list.path,
+            @input[:device_list][:device_path]
+        end 
+        
+        should "match the enabled flag" do
+          assert_equal @device_list.enabled,
+            @input[:device_list][:enabled]
+        end                               
+        
+        should "match the mount point" do
+          assert_equal @device_list.mount_point,
+            @input[:device_list][:mount_point]
+        end                                   
+        
+        should "match the mounted flag" do
+          assert_equal @device_list.mounted,
+            @input[:device_list][:mounted]
+        end
       end
+      
+      # :disk_safe_attribute_map=>{:entry=>
+      #   [{:key=>"ARCHIVING_ENABLED", :value=>true},
+      #    {:key=>"FILE_EXCLUDES_ENABLED",  :value=>true}, 
+      #    {:key=>"QUOTA_TYPE",  :value=>"NONE"}, 
+      #    {:key=>"CONTROLPANELS_ENABLED",  :value=>true}, 
+      #    {:key=>"ARCHIVE_POINT_LIMIT", :value=>"-1"},
+      #    {:key=>"RECOVERY_POINT_LIMIT", :value=>"-1"}, 
+      #    {:key=>"SOFT_QUOTA_VALUE", :value=>"-1.0"}, 
+      #    {:key=>"REPLICATION_FREQUENCY_LIMIT", :value=>"NO_LIMIT"}, 
+      #    {:key=>"HARD_QUOTA_VALUE", :value=>"-1.0"}]
+      #    }, 
+      
+      should "match the id" do
+        assert_equal @disk_safe.id, @input[:id]
+      end                           
+      
+      should "match the open flag" do
+        assert_equal @disk_safe.open, @input[:open]
+      end                                          
+      
+      should "match the recovery point count as integer" do
+        assert_equal @disk_safe.recovery_point_count,
+          @input[:recovery_point_count].to_i
+      end                                   
+      
+      should "match the size at the right byte level" do
+        assert_equal @disk_safe.size, "19.5 GB"
+      end                                 
+      
+      should "match the size of deltas at the right byte level" do
+        assert_equal @disk_safe.size_of_deltas, "17.4 GB"
+      end   
+      
+      should "match the volume id" do
+        assert_equal @disk_safe.volume_id, @input[:volume_id]
+      end
+      
     end
   end
 end
