eb9bff3695884427096a8fb5bfb5be56fae954aa
Sean Handley <sean.handley@melbourne.co.uk>
2012-03-19 10:22:29 +0000

    Tweaked client tests to match the given options passed to savon.

--- test/unit/lib/ultravault/test_client.rb
+++ test/unit/lib/ultravault/test_client.rb
@@ -6,7 +6,8 @@ class ClientTest < Test::Unit::TestCase
   context "creating a client" do
     
     setup do
-      @options = { endpoint: "some.valid.url", namespace: 'foo.bar.baz', username: "foo", password: "bar" }
+      @options = { endpoint: "some.valid.url", namespace: 'foo.bar.baz',
+                   username: "foo", password: "bar" }
       @client = UltraVault::Client.new @options
       @savon_client = @client.instance_variable_get("@client")
     end
@@ -18,18 +19,18 @@ class ClientTest < Test::Unit::TestCase
       end
       
       should "set the endpoint" do
-        assert @savon_client.wsdl.endpoint.length > 0,
-          "endpoint not set"
+        assert_equal @savon_client.wsdl.endpoint,
+          @options[:endpoint]
       end
       
       should "set the namespace" do
-        assert @savon_client.wsdl.namespace.length > 0,
-          "namespace not set"
+        assert_equal @savon_client.wsdl.namespace,
+          @options[:namespace]
       end
       
       should "set the credentials" do
         assert_equal @savon_client.http.auth.basic,
-        [@options[:username], @options[:password]]
+          [@options[:username], @options[:password]]
       end
     end
     
