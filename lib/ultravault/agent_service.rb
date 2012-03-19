5d50f2d51f5524e45e221384b25ada4fea14e8d2
Sean Handley <sean.handley@melbourne.co.uk>
2012-03-19 10:25:19 +0000

    Layout changes - keep the line width sane.

--- lib/ultravault/agent_service.rb
+++ lib/ultravault/agent_service.rb
@@ -4,12 +4,20 @@ module UltraVault
   
   class AgentService
     def initialize(params)
-      api_request = UltraVault::ApiRequest.new(host: params[:host], port: params[:port], service: :Agent, api_verison: params[:api_version], ssl: params[:ssl])
-      @client = UltraVault::Client.new endpoint: api_request.endpoint, namespace: api_request.namespace, username: params[:username], password: params[:password]
+      api_request = UltraVault::ApiRequest.new( host: params[:host],
+                                                port: params[:port],
+                                                service: :Agent,
+                                                api_verison: params[:api_version],
+                                                ssl: params[:ssl])
+      @client = UltraVault::Client.new( endpoint: api_request.endpoint,
+                                        namespace: api_request.namespace,
+                                        username: params[:username],
+                                        password: params[:password])
     end
     
     def find_agent_by_id(agent_id)
-      UltraVault::Agent.new (@client.request :getAgentByID, { :id => agent_id }).to_hash[:get_agent_by_id_response][:return]
+      UltraVault::Agent.new (@client.request :getAgentByID,
+        { :id => agent_id }).to_hash[:get_agent_by_id_response][:return]
     end
   end
   
