787f24df6e5f4c76405cbc13e732bf5d035e59f2
Sean Handley <sean.handley@melbourne.co.uk>
2012-03-19 12:02:08 +0000

    Fixed autoloading of gem classes.

--- lib/ultravault.rb
+++ lib/ultravault.rb
@@ -1,6 +1,10 @@
 module UltraVault
-  
-  autoload :Config, "ultravault/config"
-  autoload :Client, "ultravault/client"
+
+  autoload :Agent,         "./lib/ultravault/agent"
+  autoload :AgentService,  "./lib/ultravault/agent_service"
+  autoload :ApiRequest,    "./lib/ultravault/api_request"
+  autoload :Client,        "./lib/ultravault/client"
+  autoload :DiskSafe,      "./lib/ultravault/disk_safe"
+  autoload :RecoveryPoint, "./lib/ultravault/recovery_point"
   
 end
