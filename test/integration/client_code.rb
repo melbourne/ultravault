e165af533999cbe7a3f0a7d519a1bbfccd3852a2
Sean Handley <sean.handley@melbourne.co.uk>
2012-03-19 18:32:30 +0000

    Added working integration test to replace readme.

--- test/integration/client_code.rb
+++ test/integration/client_code.rb
@@ -7,13 +7,21 @@ class ClientCodeTest < Test::Unit::TestCase
   context "typical usage" do            
     
     should "retrieve agents, disksafes and recovery points" do
-      @params =  { host: 'foo.bar.baz', port: 9080,
-                   api_version: 1, ssl: false,
-                   username: 'foo', password: 'bar' }
+      # Set up the service
+      UltraVault.configure do |config|
+        config.host = 'foo.bar.baz'
+        config.port = 9080
+        config.api_version = 1
+        config.ssl = false
+        config.username = 'foo'
+        config.password = 'bar'
+      end
       # Create an agent
-      agent_service = UltraVault::AgentService.new(@params)
+      agent_service = UltraVault::AgentService.new
       agent = agent_service.find_agent_by_id("e9bd701b-dac1-4921-ab1c-467f35209e21")
       assert_not_nil agent
+      disksafes = agent.disk_safes
+      assert disksafes.count > 0
     end
   end
 end
