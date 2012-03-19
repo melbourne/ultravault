4363c9a7cad1da36acf7886802c49f59f758c2b2
Sean Handley <sean.handley@melbourne.co.uk>
2012-03-19 18:28:41 +0000

    Updated test helper to use config.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,8 +4,18 @@ require 'mocha'
 require 'webmock/test_unit'
 require 'savon'
 include WebMock::API
+require_relative '../lib/ultravault'
 
 Savon.configure do |config| 
   config.log = false
   HTTPI.log = false
+end
+
+UltraVault.configure do |config|
+  config.host = 'foo.bar.baz'
+  config.port = 9080
+  config.api_version = 1
+  config.ssl = false
+  config.username = 'foo'
+  config.password = 'bar'
 end
