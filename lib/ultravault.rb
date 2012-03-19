b7fd75ab4bef305aee268c17da354a5b90c30998
Sean Handley <sean.handley@melbourne.co.uk>
2012-03-19 18:32:16 +0000

    Updated includes for new files.

--- lib/ultravault.rb
+++ lib/ultravault.rb
@@ -1,10 +1,14 @@
 module UltraVault
 
-  autoload :Agent,         "./lib/ultravault/agent"
-  autoload :AgentService,  "./lib/ultravault/agent_service"
-  autoload :ApiRequest,    "./lib/ultravault/api_request"
-  autoload :Client,        "./lib/ultravault/client"
-  autoload :DiskSafe,      "./lib/ultravault/disk_safe"
-  autoload :RecoveryPoint, "./lib/ultravault/recovery_point"
+  require_relative "ultravault/config"
+  
+  autoload :Agent,           "./lib/ultravault/agent"
+  autoload :AgentService,    "./lib/ultravault/agent_service"
+  autoload :ApiRequest,      "./lib/ultravault/api_request"
+  autoload :Client,          "./lib/ultravault/client"
+  autoload :DiskSafe,        "./lib/ultravault/disk_safe"
+  autoload :DiskSafeService, "./lib/ultravault/disk_safe_service"
+  autoload :RecoveryPoint,   "./lib/ultravault/recovery_point"
+  autoload :SoapService,     "./lib/ultravault/soap_service"
   
 end
