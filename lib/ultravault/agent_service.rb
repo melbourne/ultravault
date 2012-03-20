726117d5065738130f4b140d2a11bd2dbfb71316
Sean Handley <sean.handley@melbourne.co.uk>
2012-03-20 11:13:06 +0000

    Added YARD doc for the three main public methods.

--- lib/ultravault/agent_service.rb
+++ lib/ultravault/agent_service.rb
@@ -2,6 +2,11 @@ module UltraVault
   
   class AgentService < UltraVault::SoapService    
     
+    # Returns an agent, if found.
+    # 
+    # @param [String] agent_id the UUID of the agent 
+    # @return [UltraVault::Agent] the agent matching the agent_id
+    # @raise [Savon::SOAP::Fault] errors from the soap transaction
     def self.find_agent_by_id(agent_id)
       response_hash = client(api_request(:Agent)).request(:getAgentByID, :id => agent_id).to_hash
       UltraVault::Agent.new(extract_agent_params(response_hash))
--- lib/ultravault/recovery_point_service.rb
+++ lib/ultravault/recovery_point_service.rb
@@ -1,6 +1,11 @@
 module UltraVault
   class RecoveryPointService < UltraVault::SoapService
-    
+
+    # Returns an array of recovery points, if found.
+    # 
+    # @param [String] disk_safe_id the UUID of the disk safe
+    # @return [[UltraVault::RecoveryPoint]] the matching recovery points for the agent
+    # @raise [Savon::SOAP::Fault] errors from the soap transaction    
     def self.find_recovery_points_by_disk_safe_id(disk_safe_id)
       response_hash = client(api_request(:RecoveryPoints)).request(:getRecoveryPoints,
         :diskSafe => {:id => disk_safe_id },
