ba6abd8a48f6193200e468692167ec535c8c92ac
Sean Handley <sean.handley@melbourne.co.uk>
2012-03-20 09:13:11 +0000

    Set disk safe service to use class methods.

--- lib/ultravault/soap_service.rb
+++ lib/ultravault/soap_service.rb
@@ -1,24 +1,17 @@
 module UltraVault
   class SoapService
     
-    def initialize(service)
-      build_api_request(service)
-      build_client
-    end
-    
-    private
-    
-    def build_api_request(service)
-      @api_request ||= UltraVault::ApiRequest.new( host: UltraVault.config.host,
+    def self.api_request(service)
+      UltraVault::ApiRequest.new( host: UltraVault.config.host,
                                   port: UltraVault.config.port,
                                   service: service,
                                   api_verison: UltraVault.config.api_version,
                                   ssl: UltraVault.config.ssl)
     end
     
-    def build_client
-      @client ||= UltraVault::Client.new( endpoint: @api_request.endpoint,
-                              namespace: @api_request.namespace,
+    def self.client(api_request)
+      UltraVault::Client.new( endpoint: api_request.endpoint,
+                              namespace: api_request.namespace,
                               username: UltraVault.config.username,
                               password: UltraVault.config.password)
     end
