8c1f4fb660af7eb78f952534a1eab2a94695b074
Sean Handley <sean.handley@melbourne.co.uk>
2012-03-20 16:36:01 +0000

    Restructured lib folder.

--- lib/ultravault.rb
+++ lib/ultravault.rb
@@ -3,14 +3,14 @@ module UltraVault
   require 'savon'
   require_relative "ultravault/config"
   
-  autoload :Agent,                "ultravault/agent"
-  autoload :AgentService,         "ultravault/agent_service"
+  autoload :Agent,                "ultravault/data_objects/agent"
+  autoload :AgentService,         "ultravault/soap_service/agent_service"
   autoload :ApiRequest,           "ultravault/api_request"
   autoload :Client,               "ultravault/client"
-  autoload :DiskSafe,             "ultravault/disk_safe"
-  autoload :DiskSafeService,      "ultravault/disk_safe_service"
-  autoload :RecoveryPoint,        "ultravault/recovery_point" 
-  autoload :RecoveryPointService, "ultravault/recovery_point_service" 
+  autoload :DiskSafe,             "ultravault/data_objects/disk_safe"
+  autoload :DiskSafeService,      "ultravault/soap_service/disk_safe_service"
+  autoload :RecoveryPoint,        "ultravault/data_objects/recovery_point" 
+  autoload :RecoveryPointService, "ultravault/soap_service/recovery_point_service" 
   autoload :SoapService,          "ultravault/soap_service"
   
 end
