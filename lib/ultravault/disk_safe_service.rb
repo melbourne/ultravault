cc1b4cf8c09e3665e2e4b319d27bbe0581d4045f
Sean Handley <sean.handley@melbourne.co.uk>
2012-03-20 11:13:06 +0000

    Added YARD doc for the three main public methods.

--- lib/ultravault/disk_safe_service.rb
+++ lib/ultravault/disk_safe_service.rb
@@ -1,7 +1,12 @@
 module UltraVault
   
   class DiskSafeService < UltraVault::SoapService
-    
+
+    # Returns an array of disk safes, if found.
+    # 
+    # @param [String] agent_id the UUID of the agent 
+    # @return [[UltraVault::DiskSafe]] the matching disk safes for the agent
+    # @raise [Savon::SOAP::Fault] errors from the soap transaction    
     def self.find_disksafes_by_agent_id(agent_id)
       response_hash = client(api_request(:DiskSafe)).request(:getDiskSafesForAgent, :agent => {:id => agent_id }).to_hash
       extract_disk_safe_params(response_hash).collect do |disk_safe|
