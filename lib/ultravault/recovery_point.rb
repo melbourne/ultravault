21afd53ac0f9958185bb2fa594a62bbef2382d66
Sean Handley <sean.handley@melbourne.co.uk>
2012-03-20 10:00:42 +0000

    Added recovery point service and test.

--- lib/ultravault/recovery_point.rb
+++ lib/ultravault/recovery_point.rb
@@ -5,7 +5,7 @@ module UltraVault
     
     def initialize params
       @agent_id = params[:agent_id]
-      @created_at = Time.at(params[:created_on_timestamp_in_millis] / 1000.0)
+      @created_at = Time.at(params[:created_on_timestamp_in_millis].to_i / 1000.0)
       @disk_safe_id = params[:disk_safe_id]
       @id = params[:recovery_point_id]
       @state = params[:recovery_point_state].downcase
