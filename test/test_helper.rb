d80b4e62ee8a238927fa7b7dba2897908bb64833
Sean Handley <sean.handley@melbourne.co.uk>
2012-03-21 09:52:23 +0000

    Set helper to be able to load all client code separately.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,21 @@ require 'mocha'
 require 'webmock/test_unit'
 require 'savon'
 include WebMock::API
-require_relative '../lib/ultravault'
+
+require_relative '../lib/ultravault/api_request'
+require_relative '../lib/ultravault/client'
+require_relative '../lib/ultravault/config'
+require_relative '../lib/ultravault/data_objects/agent'
+require_relative '../lib/ultravault/data_objects/disk_safe'
+require_relative '../lib/ultravault/data_objects/recovery_point'
+require_relative '../lib/ultravault/soap_service'
+require_relative '../lib/ultravault/soap_service/agent_service'
+require_relative '../lib/ultravault/soap_service/disk_safe_service'
+require_relative '../lib/ultravault/soap_service/recovery_point_service'
+
+require_relative 'support/fixtures'
+
+include UltraVault::Fixtures
 
 UltraVault.configure do |config|
   config.host = 'foo.bar.baz'
