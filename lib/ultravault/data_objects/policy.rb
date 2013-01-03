aebf46cef22479f222077bb8c0d664a4b52aca8c
Andrew Turner <andrew.turner@melbourne.co.uk>
2013-01-03 10:55:24 +0000

    Don't try and find by name here Let the API consumer find the relevant UUID and use that

--- lib/ultravault/data_objects/policy.rb
+++ lib/ultravault/data_objects/policy.rb
@@ -1,10 +1,21 @@
 module UltraVault
   class Policy < OpenStruct
 
-    def self.find_by_name(name)
-      UltraVault::PolicyService.new.all_policies.each do |policy|
-        return policy if policy.name == name
-      end
+    # Returns an agent, if found.
+    # 
+    # @param [String] policy_id the UUID of the policy 
+    # @return [UltraVault::Policy] the policy matching the policy_id
+    # @raise [Savon::SOAP::Fault] errors from the soap transaction
+    def self.find_by_id(policy_id)
+      UltraVault::PolicyService.new.find_policy_by_id(policy_id)
+    end
+
+    # Returns all policies for the current user.
+    # 
+    # @return [[UltraVault::Policy]] current user's policies
+    # @raise [Savon::SOAP::Fault] errors from the soap transaction    
+    def self.all
+      UltraVault::PolicyService.new.all_policies
     end
     
   end
