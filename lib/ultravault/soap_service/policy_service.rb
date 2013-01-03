5d04ccf8d98a1a86411e3ef560523ba1f6186691
Andrew Turner <andrew.turner@melbourne.co.uk>
2013-01-03 12:37:13 +0000

    Policy service method for finding by id

--- lib/ultravault/soap_service/policy_service.rb
+++ lib/ultravault/soap_service/policy_service.rb
@@ -6,6 +6,12 @@ module UltraVault
       super(:Policy)
     end
     
+    def find_policy_by_id(policy_id)
+      response_hash = client.request(:getPolicyById, :id => policy_id).to_hash
+      params = extract_params(response_hash, :get_policy_by_id_response)
+      UltraVault::Policy.new(params)
+    end
+    
     def all_policies
       response_hash = client.request(:getPolicies).to_hash
       params = extract_params_array(response_hash,
