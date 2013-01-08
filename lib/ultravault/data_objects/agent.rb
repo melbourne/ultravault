886327c730f7005fe1360b4ab2a1e7c148fbd26c
Andrew Turner <andrew.turner@melbourne.co.uk>
2013-01-08 12:52:22 +0000

    compact array of disk safes

--- lib/ultravault/data_objects/agent.rb
+++ lib/ultravault/data_objects/agent.rb
@@ -17,7 +17,7 @@ module UltraVault
     # @raise [Savon::SOAP::Fault] errors from the soap transaction    
     def disk_safes
       if UltraVault.config.api_version > 1
-        @disk_safes ||= UltraVault::DiskSafe.all.map { |disk_safe| disk_safe if disk_safe.agent_id == id }
+        @disk_safes ||= UltraVault::DiskSafe.all.map { |disk_safe| disk_safe if disk_safe.agent_id == id }.compact
       else
         @disk_safes ||= UltraVault::DiskSafe.find_all_by_agent_id(id)
       end
