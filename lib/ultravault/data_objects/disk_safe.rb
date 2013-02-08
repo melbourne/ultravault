abeca553be23e2d584087ba1a5c0d5360c684011
Andrew Turner <andrew.turner@melbourne.co.uk>
2013-02-08 16:48:03 +0000

    bye bye evil eval

--- lib/ultravault/data_objects/disk_safe.rb
+++ lib/ultravault/data_objects/disk_safe.rb
@@ -2,7 +2,7 @@ require_relative 'v1/disk_safe'
 require_relative 'v2/disk_safe'
 
 module UltraVault
-  klass = eval "UltraVault::DiskSafeV" + UltraVault.config.api_version.to_s
+  klass = UltraVault.const_get("DiskSafeV#{UltraVault.config.api_version.to_s}")
   class DiskSafe < klass
     def initialize(params)
       super
